4720ce3aa9ce1544cb7e2fade22551fb5e66dc2d
vasyaste <vasiliy.stepanov@devexpress.com>
2022-12-09 11:11:26 +0400

    Upgrade to 22.2

--- CS/Form1.cs
+++ CS/Form1.cs
@@ -10,11 +10,10 @@ using DevExpress.XtraPrintingLinks;
 using System.Diagnostics;
 using System.Drawing.Imaging;
 using System.IO;
+using DevExpress.Drawing;
 
-namespace ChartManualReport
-{
-    public partial class Form1 : Form
-    {
+namespace ChartManualReport {
+    public partial class Form1 : Form {
         private int pageWidthInPixels;
         private int pageHeightInPixels;
 
@@ -22,8 +21,7 @@ namespace ChartManualReport
         MemoryStream ms;
         Link link;
 
-        public Form1()
-        {
+        public Form1() {
             InitializeComponent();
 
             ms = new MemoryStream(1000);
@@ -34,8 +32,7 @@ namespace ChartManualReport
             chartBitmap = new Metafile(ms);
         }
 
-        private void simpleButton1_Click(object sender, EventArgs e)
-        {
+        private void simpleButton1_Click(object sender, EventArgs e) {
             link = new Link(printingSystem1);
 
             link.BeforeCreateAreas += new EventHandler(link_BeforeCreateAreas);
@@ -44,21 +41,18 @@ namespace ChartManualReport
             link.ShowPreviewDialog();
         }
 
-        void link_BeforeCreateAreas(object sender, EventArgs e)
-        {
+        void link_BeforeCreateAreas(object sender, EventArgs e) {
             RecalcSize();
         }
 
-        void link_CreateDetailArea(object sender, CreateAreaEventArgs e)
-        {
+        void link_CreateDetailArea(object sender, CreateAreaEventArgs e) {
             RecalcSize();
 
-            if (pageWidthInPixels < 100 || pageHeightInPixels < 100)
-            {
+            if (pageWidthInPixels < 100 || pageHeightInPixels < 100) {
                 e.Graph.StringFormat = new BrickStringFormat(StringAlignment.Center);
-                
+
                 e.Graph.DrawString("Very small size", Color.Red,
-                    new RectangleF(0, pageHeightInPixels/2, pageWidthInPixels, pageHeightInPixels/2), BorderSide.None);
+                    new RectangleF(0, pageHeightInPixels / 2, pageWidthInPixels, pageHeightInPixels / 2), BorderSide.None);
 
                 e.Graph.BackColor = Color.Transparent;
                 e.Graph.DrawLine(new PointF(0, 0), new PointF(pageWidthInPixels, pageHeightInPixels), Color.Red, 2);
@@ -76,21 +70,18 @@ namespace ChartManualReport
                 new RectangleF(0, size.Height, pageWidthInPixels, pageHeightInPixels - size.Height));
         }
 
-        private void printingSystem1_AfterMarginsChange(object sender, MarginsChangeEventArgs e)
-        {
+        private void printingSystem1_AfterMarginsChange(object sender, MarginsChangeEventArgs e) {
             RecalcSize();
             link.CreateDocument();
         }
 
-        private void printingSystem1_PageSettingsChanged(object sender, EventArgs e)
-        {
+        private void printingSystem1_PageSettingsChanged(object sender, EventArgs e) {
             RecalcSize();
             link.CreateDocument();
         }
 
-        private void RecalcSize()
-        {
-            printingSystem1.Graph.PageUnit = GraphicsUnit.Pixel;
+        private void RecalcSize() {
+            printingSystem1.Graph.PageUnit = DXGraphicsUnit.Pixel;
 
             pageWidthInPixels = Convert.ToInt32(Math.Floor(printingSystem1.Graph.ClientPageSize.Width));
             pageHeightInPixels = Convert.ToInt32(Math.Floor(printingSystem1.Graph.ClientPageSize.Height));
